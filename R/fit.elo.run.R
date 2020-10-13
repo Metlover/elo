2b32415f9cc47b375615a9f55320040e7fd52081
John Edwards <edwards1860@gmail.com>
2020-10-12 23:33:46 -0700

    Added a cross-validation function to supplement fit.elo.run

--- R/fit.elo.run.R
+++ R/fit.elo.run.R
@@ -117,6 +117,96 @@ fit.elo.run <- function(formula, data, optimfun = 'mse', gridsearch, ...){
   return(run)
 }
 
+#' Use rolling-origin cv to grid search for optimal elo.run parameters
+#'
+#' Takes in the arguments of the given elo.run function along with an optimization
+#' function and a grid of parameters, then performs rolling origin cross validation
+#' to determine the optimal parameters.
+#' @inheritParams fit.elo.run
+#' @param nfolds Indicates how many folds should be used in cross-validation.
+#' Either this value or \code{groups} should be specified. See details for more
+#' information.
+#' @param groups Indicates what groups should be used in cross-validation.
+#' Either this value or \code{nfolds} should be specified. See details for more
+#' information.
+#' @param new.team.elo An optional argument indicating how new teams (teams not
+#' in the training set but in the test set) should be viewed from an elo
+#' perspective. Defaults to 1500. See details for more information.
+#' @param ... Other arguments (not used at this time).
+#' @return An elo object optimized by the given optimization function and
+#' cross-validated.
+#' @details \code{optimfun} refers to one of the following optimization functions
+#' built into the elo package. 'mse' or 'brier' will train on minimizing the
+#' mean squared error of the model, 'accuracy' will train on maximizing the
+#' accuracy of the mode, 'calibration' will train on getting the calibration score
+#' of the model as close to 1 as possible, and 'auc' will train on maximizing the
+#' auc of the model.
+#'
+#' \code{gridsearch} is a dataframe of parameter values that \code{fit.elo} will
+#' search through to optimize. The dataframe's column names correspond to the
+#' parameters to be optimized within the formula (i.e. if the term
+#' \code{adjust(team.home,hfa)} is included, to optimize hfa you will need a
+#' column named 'hfa' with plausible values in the grid search dataframe.
+#' A column named "k" must be included. If you wish to optimize other parameters
+#' while holding k constant, simply set k to the same value for all rows of the
+#' grid search.
+#'
+#' \code{nfolds} indicates how many folds should be used in cross-validation.
+#' This function divides \code{data} into \code{nfolds} for cv.
+#'
+#' \code{groups} indicates the groups to be used for folds for cross-validation.
+#' These should be unique identifiers for each group, such as a string indicating
+#' which tournament a game took place in or the season where a game took place.
+#'
+#' For those unfamiliar with how rolling-origin cv works, the function first
+#' splits the dataset into folds using either \code{nfolds} or \code{groups}.
+#' Using the parameters in the grid-search, this function generates elo values
+#' using the first fold, then tests those elo values on the second fold.
+#' It then repeats the process but combines the first and second folds as the
+#' training set, and tests on the third fold. It repeats this process until all
+#' the folds except the first have been tested, then repeats *this* process until
+#' all parameters have been tested. It then evaluates each set of parameters on
+#' the simple average of the optimization.
+#'
+#' To walk through an example, imagine that we were fitting elo parameters on a
+#' dataframe over the past 5 season's worth of games of a professional sports
+#' league, and I have specified the season as the grouping factor for
+#' cross-validation. Using the first set of parameters in the grid-search,
+#' \code{cv.elo.run} fits an elo model on the first season, then takes those
+#' final elo values and uses them to predict the outcomes of the second season's
+#' matches, then gathers summary metrics on those results. Then, using the same
+#' parameters, \code{cv.elo.run} trains an elo.run on the first and second
+#' season, then evaluates on the third season, and so on. Once the second
+#' through fifth seasons have been evaluated, the process repeats with a new
+#' set of parameters.
+#'
+#' \code{new.team.elo} is used for evaluating new teams. Sometimes new teams
+#' will join tournaments or games, especially over large datasets or small datasets
+#' with a lot of folds (the latter, by the way, is not recommended), to handle
+#' those instances, those new teams are treated as having an elo of 1500 unless
+#' specified otherwise by \code{new.team.elo}. For example, an expansion team
+#' joining a professional sports league will tend to perform worse than the
+#' inaugural teams initially, and hence, a \code{new.team.elo} value lower than
+#' 1500 may be more predictive.
+#'
+#'
+#' @examples
+#' data(tournament)
+#' params = expand.grid(k = seq(1,250,by=5),
+#'                      hfa = seq(0,100,by=5))
+#' cv.elo.run(score(points.Home, points.Visitor) ~ adjust(team.Home, hfa) + team.Visitor,
+#' data = tournament,
+#' optimfun = 'mse',
+#' gridsearch = params,
+#' nfolds = 3)
+#' @name cv.elo.run
+NULL
+#> NULL
+
+#' @rdname cv.elo.run
+#' @export
+
+
 cv.elo.run <- function(formula, data, optimfun = 'mse', gridsearch, nfolds = NULL,
                        groups = NULL, new.team.elo = 1500, ...){
 
@@ -144,7 +234,7 @@ cv.elo.run <- function(formula, data, optimfun = 'mse', gridsearch, nfolds = NUL
     adj_formula = formula(do.call("substitute",list(formula, parameters)))
 
     if(!is.null(nfolds)){
-      rows = nrows(data)
+      rows = nrow(data)
       fold_size <- ceiling(rows / nfolds)
       temp = split(data, rep(1:ceiling(rows/fold_size), each=fold_size, length.out=rows))
       nfolds.temp = nfolds
@@ -155,7 +245,6 @@ cv.elo.run <- function(formula, data, optimfun = 'mse', gridsearch, nfolds = NUL
     } else {
       stop("No folds or groups provided. Please input a value for nfolds or groups and try again.")
     }
-    denom = 0
 
     for(m in c(2:nfolds.temp)){
       train = do.call("rbind", temp[c(1:m-1)])
@@ -163,6 +252,8 @@ cv.elo.run <- function(formula, data, optimfun = 'mse', gridsearch, nfolds = NUL
 
       train.run = suppressWarnings(elo.run(formula = adj_formula, data = train, k = k, ...))
       test.run = suppressWarnings(elo.run(formula = adj_formula, data = test, k = k, ...))
+
+      browser()
 
       elos = final.elos(train.run)
       for(team in test.run$teams){
@@ -176,28 +267,26 @@ cv.elo.run <- function(formula, data, optimfun = 'mse', gridsearch, nfolds = NUL
       results <- (pred >0.5 & test.run$elos[,4] == 1) |
         (pred < 0.5 & test.run$elos[,4] == 0) |
         (pred == 0.5 & test.run$elos[,4] == 0.5)
-      gridsearch$accuracy[n] = gridsearch$accuracy[n] + (sum(results) / length(results)) * nrow(train)
+      gridsearch$accuracy[n] = gridsearch$accuracy[n] + (sum(results) / length(results))
 
       x.won <- pred[test.run$elos[,4] == 1]
       x.lost <- pred[test.run$elos[,4] == 0]
 
       n.won <- 0 + length(x.won) # to coerce to double
       if(n.won == 0 || length(x.lost) == 0) stop("Unable to calculate AUC: need both 0 and 1 in the wins column.")
-      gridsearch$auc[n] = gridsearch$auc[n] + ((sum(rank(c(x.won, x.lost))[1:n.won]) - n.won*(n.won + 1)/2)/(n.won * length(x.lost))) * nrow(train)
+      gridsearch$auc[n] = gridsearch$auc[n] + ((sum(rank(c(x.won, x.lost))[1:n.won]) - n.won*(n.won + 1)/2)/(n.won * length(x.lost)))
 
       pred_wins <- pred[which(score(pred,0.5) == 1)]
       correct <- score(pred,0.5) & test.run$elos[,4]
-      gridsearch$calibration[n] = gridsearch$calibration[n] + (sum(pred_wins) / sum(correct)) * nrow(train)
+      gridsearch$calibration[n] = gridsearch$calibration[n] + (sum(pred_wins) / sum(correct))
 
       r <- test.run$elos[,4] - pred
-      gridsearch$mse[n] = gridsearch$mse[n] + mean(r^2) * nrow(train)
-
-      denom = denom + nrow(train)
+      gridsearch$mse[n] = gridsearch$mse[n] + mean(r^2)
     }
-    gridsearch$mse[n] = gridsearch$mse[n] / denom
-    gridsearch$accuracy[n] = gridsearch$accuracy[n] / denom
-    gridsearch$calibration[n] = gridsearch$calibration[n] / denom
-    gridsearch$auc[n] = gridsearch$auc[n] / denom
+    gridsearch$mse[n] = gridsearch$mse[n] / (nfolds.temp - 1)
+    gridsearch$accuracy[n] = gridsearch$accuracy[n] / (nfolds.temp - 1)
+    gridsearch$calibration[n] = gridsearch$calibration[n] / (nfolds.temp - 1)
+    gridsearch$auc[n] = gridsearch$auc[n] / (nfolds.temp - 1)
   }
 
   if(!(tolower(optimfun) %in% c('mse','brier','accuracy',
